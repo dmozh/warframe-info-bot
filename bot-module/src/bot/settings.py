1567f642724ce1f837227269e8ecbdbbcbe93459
Danila <dmozh58@gmail.com>
2021-07-09 16:57:17 +0300

    add gitignore

--- bot-module/src/bot/settings.py
+++ bot-module/src/bot/settings.py
@@ -1,6 +1,6 @@
 # временное решение
 class Settings:
-    token = 'Njg2MjE5ODI5MzM3Nzg0MzMw.XmUCBA.yuZhUdXFSTii9pGhwnz1gYoqyDA'
+    token = 'token'
     bot = 'WarframeBot-info'
     id = 686219829337784330
     prefix = '**'
