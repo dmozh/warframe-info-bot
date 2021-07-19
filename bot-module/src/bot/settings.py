6da3ca95ef5f69e77d543898257427497c260750
Danila <dmozh58@gmail.com>
2021-07-19 22:28:54 +0300

    create trade command todo REFACTOR,REFACTOR,REFACTOR,REFACTOR,REFACTOR

--- bot-module/src/bot/settings.py
+++ bot-module/src/bot/settings.py
@@ -1,6 +1,6 @@
 # временное решение
 class Settings:
-    token = 'Njg2MjE5ODI5MzM3Nzg0MzMw.XmUCBA.-Pz4eG1BZshbpauHfec5530A3ro'
+    token = ''
     bot = 'WarframeBot-info'
     id = 686219829337784330
     prefix = '$'
